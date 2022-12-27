64e6ca747d322d59f05a3f7eb1edf525df4906f9
Luciano Barreira <luc.s.barreira@gmail.com>
2022-12-27 14:22:10 +0100

    Add missing $ to variable references

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.name
-  cidr = "{var.environment.prefix}.0.0/16"
+  cidr = "${var.environment.prefix}.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["{var.environment.prefix}.101.0/24", "{var.environment.prefix}.102.0/24", "{var.environment.prefix}.103.0/24"]
+  public_subnets  = ["${var.environment.prefix}.101.0/24", "${var.environment.prefix}.102.0/24", "${var.environment.prefix}.103.0/24"]
   
   enable_nat_gateway = true
 
@@ -35,7 +35,7 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.7.0"
 
-  name = "{var.environment.name}-blog"
+  name = "${var.environment.name}-blog"
 
   min_size = var.min_size
   max_size = var.max_size
@@ -52,7 +52,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "{var.environment.name}-blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -62,7 +62,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "{var.environment.name}-"
+      name_prefix      = "${var.environment.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
