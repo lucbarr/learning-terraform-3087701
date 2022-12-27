d88d6a2ff7c4c65396c5292b8f20558b1d51abaa
Luciano Barreira <luc.s.barreira@gmail.com>
2022-12-27 11:19:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog_new"
   
-  vpc_id = module.vpc.public_subnets
+  vpc_id = module.vpc.public_subnets[0]
   
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
